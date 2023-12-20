99005ed7eebcb4761e80431332bed4db92e9c1d7
Erik Pilz <erik@delighted.com>
2023-12-20 14:24:55 -0800

    Fixes typo
    Moving too fast...

--- lib/jasmine/asset_expander.rb
+++ lib/jasmine/asset_expander.rb
@@ -13,7 +13,7 @@ module Jasmine
     UnsupportedRailsVersion = Class.new(StandardError)
 
     def asset_bundle
-      return Rails4Or5Or6or7AssetBundle.new if Jasmine::Dependencies.rails4? || Jasmine::Dependencies.rails5? || Jasmine::Dependencies.rails6? || Jasmine::Dependencies.rails7?
+      return Rails4Or5Or6Or7AssetBundle.new if Jasmine::Dependencies.rails4? || Jasmine::Dependencies.rails5? || Jasmine::Dependencies.rails6? || Jasmine::Dependencies.rails7?
       raise UnsupportedRailsVersion, "Jasmine only supports the asset pipeline for Rails 4. - 7"
     end
 
